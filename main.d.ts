686928f10f5fc5518e6d145197b8136ed6b7cb1c
Michael Mauracher <mauracher.michael@gmail.com>
2016-12-13 13:33:10 +0100

    NewVersion

--- main.d.ts
+++ main.d.ts
@@ -1,22 +1,20 @@
-// Type definitions for alanning:roles.
-// Project: https://atmospherejs.com/alanning/roles
-// Definitions by:
-// Dave Allen <https://github.com/fullflavedave>
+declare module "meteor/alanning:roles" {
+    import { ObservableCursor } from 'meteor-rxjs';
+    module Roles {
+      function createRole(roleName: string): string;
+      function deleteRole(roleName: string): void;
+      function addUsersToRoles(users: any, roles: any, groups?: string): void;
+      function removeUsersFromRoles(users: any, roles: any): void;
+       // User can be user ID or user object.
+      function userIsInRole(user: any, roles: any): boolean;
+      function getRolesForUser(userId: string): string[];
+      function getAllRoles(): ObservableCursor<RolesDAO>;
+      function getUsersInRole(roleName: string): ObservableCursor<RolesDAO>;
+      var GLOBAL_GROUP: string;
+    }
 
-interface RolesDAO {
-  _id?: string;
-  name?: string;
-}
-
-declare module Roles {
-  function createRole(roleName: string): string;
-  function deleteRole(roleName: string): void;
-  function addUsersToRoles(users: any, roles: any, groups?: string): void;
-  function removeUsersFromRoles(users: any, roles: any): void;
-   // User can be user ID or user object.
-  function userIsInRole(user: any, roles: any): boolean;
-  function getRolesForUser(userId: string): string[];
-  function getAllRoles(): Mongo.Cursor<RolesDAO>;
-  function getUsersInRole(roleName: string): Mongo.Cursor<RolesDAO>;
-  var GLOBAL_GROUP: string;
+    interface RolesDAO {
+      _id?: string;
+      name?: string;
+    }
 }
